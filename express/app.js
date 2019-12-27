2aa10061c28b028a97221209476e1db0cacec17d
Eisele, Marcus (415) <marcus.eisele@daimler.com>
2019-12-27 17:35:36 +0100

    Add redis to express

--- express/app.js
+++ express/app.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const redis = require("redis");
 const app = express();
 const port = 3010;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// redisDemo.js
+let redisClient = redis.createClient(); // this creates a new client
+
+redisClient.on("connect", function() {
+  console.log("Redis client connected");
+  redisClient.set("my test key", "my test value", redis.print);
+
+  for (let i = 0; i < 1000000; i++) {
+    redisClient.set("my test key" + i, "my test value", console.log(i));
+  }
+});
+
+redisClient.on("error", function(err) {
+  console.log("Something went wrong " + err);
+});
+
+app.get("/", (req, res) => {
+  redisClient.get("my test key", function(error, result) {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+    // console.log("GET result ->" + result);
+    res.send("Hello World: " + result);
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
